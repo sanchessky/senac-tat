89084c6f949e4fa26ff5f382b1621dd97d65cd3e
Thiago Sanches <115747016+sanchessky@users.noreply.github.com>
2024-10-22 16:11:57 -0300

    Create hello.c

---
+++ UC9/Desenvolvimento/C++/hello.c
@@ -0,0 +1,19 @@
+// IMPORTAÇÃO DA BIBLIOTECA PADRÃO PARA O USO DE TECLADO E MONITOR (Ctrl + /)
+#include <stdio.h>
+
+int main(){
+   
+    /* Meu Primeiro Código em linguagem C */
+
+    printf ("Hello, World! \n");
+
+// Para exibir uma mensagem na tela iremos usar o comando printf(exibir formatando)//
+
+    return 0;
+
+// informar ao compilador que o programa acabou (return) 
+}
+
+
+
+// Para compilar é (gcc hello.c -o hello.exe)  para é executar ./hello.exe
