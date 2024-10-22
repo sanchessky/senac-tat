f723680c149cbaa6cc2d1f19ff71c9b8d07ee92c
Thiago Sanches <115747016+sanchessky@users.noreply.github.com>
2024-10-22 16:12:36 -0300

    Create hello2.c

---
+++ UC9/Desenvolvimento/C++/hello2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    // Adicionar um pequeno cabeçalho
+    printf("---------------Programa Mensagem-----------------");
+    printf("\n---------------Seja Bem-vindo-----------------\nl");
+    printf("----------------------Fim-----------------");
+
+    return 0;
+}
+
+
