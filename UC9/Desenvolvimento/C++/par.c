964f8dc3940d476e1edac353c4337eaf84edd962
Thiago Sanches <115747016+sanchessky@users.noreply.github.com>
2024-10-29 15:15:23 -0300

    Create par.c

---
+++ UC9/Desenvolvimento/C++/par.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+    // este programa obtem um numero de usuario e diz se é par, exibindo uma mensagem em tela //
+int main() {
+    int n;
+    printf("Digite um Número e tecle Enter \n");
+    scanf("%d",&n);
+
+    // Se n for divido por 2 o resultado é par // resto é igual a %
+    if (n % 2 == 0){
+        printf("O valor %d é par \n", n);
+
+    }
+
+
+
+
+
+    return 0;
+}
