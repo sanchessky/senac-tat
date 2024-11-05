f01b4f24db344fef48b4fdbe3109faf23d72276b
Thiago Sanches <115747016+sanchessky@users.noreply.github.com>
2024-11-05 17:21:44 -0300

    Create texto1.c

---
+++ UC9/Desenvolvimento/C++/texto1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    //declarar uma variavel para armazenar o primeiro nome de alguem//
+    char nome[7] = ("thiagos");  //char rebebe 1 caracteer vc tem que definir o tamanho [6] = (thiago)//
+    printf("%s\n",nome);
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
