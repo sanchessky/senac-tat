deba93c7afba108779b77897a599ea4b9d7e0f85
Thiago Sanches <115747016+sanchessky@users.noreply.github.com>
2024-11-12 17:01:15 -0300

    Create ativ1.c

---
+++ UC9/Desenvolvimento/C++/ativ1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "../lib/atividade.h" 
+
+    
+    int main(){
+        ##OS CODIGOS ESTAO COMENTADOS, PARA USAR BASTA DESCOMENTAR##
+    //int n1,n2; 
+    //printf("Digite o primeiro número\n");
+    //scanf("%d",&n1);
+    //printf("Digite o segundo número\n");
+    //scanf("%d",&n2);
+    //printf("%d\n",maior(n1,n2));
+    //return 0;
+    
+
+    //char letra;
+    //printf("Digite M-Masculino ou F-Feminino\n");
+    //scanf("%c",&letra);
+    //printf("Masculino\n");
+    //scanf
+
+
+//return 0;
+
+//char letra;
+//printf ("Digite uma letra\n");
+//scanf("%c",&letra);
+//vogal(letra);
+
+//return 0;
+
+
+//float sal;
+//printf("Digite o salário do funcionários:\n");
+//scanf("%f",&sal);
+//reajuste(sal);
+
+//return 0;
+
+}
