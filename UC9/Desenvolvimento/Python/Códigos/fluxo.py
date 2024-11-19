76f061ed55d76711a009126271be54d6ca7664e6
Thiago Sanches <115747016+sanchessky@users.noreply.github.com>
2024-11-19 14:35:11 -0300

    Add files via upload

---
+++ UC9/Desenvolvimento/Python/Códigos/fluxo.py
@@ -0,0 +1,6 @@
+#desvio de fluxo 
+valor = int(input("Digite um número: "))
+if valor % 2 ==0:
+    print(f"O valor {valor} digitado é par")
+else:
+    print(f'O valor {valor} digitado é impar')
---
+++ UC9/Desenvolvimento/Python/Códigos/operacoes.py
@@ -0,0 +1,14 @@
+#operações matemática
+n1 = int(input("Digite um número:"))
+n2 = int(input("Digite outro número:"))
+soma = n1 + n2
+subtrair = n1 - n2
+multiplicar = n1 * n2
+dividir = n1 / n2
+resto = n1 % n2
+print(f"O resultado da Soma é {soma} o tipo da variável é {type(soma)} ")
+print(f"O resultado da Subtração é {subtrair} ")
+print(f"O resultado da Multiplicação é {multiplicar}")
+print(f"O resultado da Divisão é {dividir} o tipo da variável é {type(dividir)}")
+print(f"O resultado da Resto é {resto}")
+
